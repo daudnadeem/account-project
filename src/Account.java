a197510a79e418a4c71a0dbd91f65eaae89bf0a3
Daud Nadeem <d.nadeem93@gmail.com>
2018-10-30 10:52:07 +0000

    Update src/Account.java

---
+++ src/Account.java
@@ -0,0 +1,14 @@
+
+public class Account {
+	
+	protected String firstName;
+	protected String lastName;
+	protected int accountNumber;
+
+	public Account(String firstName, String lastName, int accountNumber) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountNumber = accountNumber;
+	}
+}
