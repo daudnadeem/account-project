1b7838cfde27c5b4b5588b7ace05a72d8947f1c8
Daud Nadeem <d.nadeem93@gmail.com>
2018-10-30 11:00:41 +0000

    account & account manager

---
+++ src/AccountManager.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class AccountManager {
+	
+	protected HashMap<String, Integer> myMap = new HashMap<String, Integer>();
+	protected String name;
+	
+	public void addAccount(Account acc) {
+		name = acc.getFirstName() + acc.getLastName();
+		myMap.put(name, acc.getAccountNumber());
+	}
+
+	public HashMap<String, Integer> getMyMap() {
+		return myMap;
+	}
+
+}
